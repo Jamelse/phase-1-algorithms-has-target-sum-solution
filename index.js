a150c1c0f396b56b1af323622e0c6ac89ed320d2
Jacob <jacobamelse@hotmail.com>
2022-08-12 20:08:01 -0500

    commit tree 7afcbdd92f330f124046f584604cc9e303c62bee

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let map = {}
+for (const result of array) {
+    const num = target - result;
+    if (map[num])
+      return true
+      map[result] = true;
+     }
+  return false; 
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+  0(n)??
 */
 
 /* 
-  Add your pseudocode here
+  create object. iterate over array. with current num, identify a number-
+  - that adds up to target. 
 */
 
 /*
-  Add written explanation of your solution here
+ Create an object that nests an array of numbers. Iterate over that array
+ and create a 'result number' that adds to our target. If the keys in this
+ object is the result of our current number, we return true and save that number as key.
+ Otherwise, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log('Expecting true');
+  console.log('=>', hasTargetSum([1,2,3,4,5], 6));
+
+  console.log('');
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
